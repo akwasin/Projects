c5812dadbc340d51f7abb1257231f547dda76493
akwasin <akwasiijunior@gmail.com>
2015-12-14 09:21:57 +0100

    Enum menu

---
+++ Forelasning/Forelasning 14/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forelasning_14
+{
+    enum Menu
+    {
+        Add = 1, Subtract, Multiply, Divide, End
+            // = 1 får menyn att börja på 1
+    }
+}

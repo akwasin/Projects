e7e254b6b403e8fd92a073b0305e10bd0637d1be
akwasin <akwasiijunior@gmail.com>
2015-12-15 10:13:32 +0100

    Med föreläsning 15 på felhantering

---
+++ Forelasning/Förelasning 15/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Förelasning_15
+{
+    class Class1
+    {
+
+        internal void Method()
+        {
+            try
+            {
+                var tal = 67 / int.Parse(Console.ReadLine());
+                // division med 0 ger en crash devide by zero exception
+            }
+            catch (Exception ex)
+            {               
+                //throw new Exception($"Message {ex.Message}\r\nSource:{ex.Source}\r\nSource:{ex.StackTrace}");
+                Console.WriteLine(ex.Message);
+                Method();
+            }
+            Console.WriteLine("Metoden i klassen");
+            
+        }
+    }
+}
---
+++ Forelasning/Förelasning 15/Class2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Förelasning_15
+{
+    class Class2
+    {
+        internal void Method()
+        {
+            try
+            {
+                Console.WriteLine("Skriv ett tal som ska delas med 6");
+                var tal = 6/int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex) // denna är redundant (den gör alla exceptions annars)
+            {
+                Console.WriteLine("Hanterar allt");
+                throw ex;
+            }
+            finally
+            {
+                Console.WriteLine("STÄNG Databasen!!!");
+            }
+            Console.WriteLine("Eftersläntare");
+
+        }
+        }
+    }
+}

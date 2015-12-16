431a83203c90ad3a4832953470b48d6fa619e005
akwasin <akwasiijunior@gmail.com>
2015-12-16 15:11:13 +0100

    Lab6

---
+++ laborationAkwasiKarikari/Lab6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float newFloat = 10f;
+            long newLong = 1000;
+            char newChar = (char)100;
+            string newString = "hej";
+            ulong newUlong = 10000u;
+
+            int Ett = 7;
+            int Tva = 13;
+
+            char c = Convert.ToChar(newFloat);
+            int newInt = Convert.ToInt32(newFloat);
+
+
+
+        }
+    }
+}

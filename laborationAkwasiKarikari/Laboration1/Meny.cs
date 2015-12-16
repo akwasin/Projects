b0dc3cc47bc038a416889827a5db20feec654b52
akwasin <akwasiijunior@gmail.com>
2015-12-16 14:02:56 +0100

    Lagt till meny.cs

---
+++ laborationAkwasiKarikari/Laboration1/Meny.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration1
+{
+    static class Meny
+    {
+        public static void SkapaMeny()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - Skapa vara");
+            Console.WriteLine("2 - Inventera vara");
+            Console.WriteLine("3 - Lista varor");
+            Console.WriteLine("4 - Avsluta");
+        }
+    }
+}

ed25f70db0551dbf09846e25aea8b9ba910d2342
akwasin <akwasiijunior@gmail.com>
2015-12-28 12:14:52 +0100

    Övn6

---
+++ Kurs C#/laborationAkwasiKarikari/Lab6/MyDoubleType.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class MyDoubleType
+    {
+        public double DoubleValue { get; set; }
+        public MyDoubleType(double doubleValue)
+        {
+            DoubleValue = doubleValue;
+        }
+        public static MyDoubleType operator +(MyDoubleType myDouble, double tal)
+        {
+            var result = new MyDoubleType(myDouble.DoubleValue + tal);
+            return result;
+        }
+        public static MyDoubleType operator -(MyDoubleType myDouble, double tal)
+        {
+            var result = new MyDoubleType(myDouble.DoubleValue - tal);
+            return result;
+        }
+        public static MyDoubleType operator /(MyDoubleType myDouble, double tal)
+        {
+            var result = new MyDoubleType(myDouble.DoubleValue / tal);
+            if (tal == 0)
+            {
+                throw new Exception("Can't be divided by zero");
+            }
+            else
+                return result;
+        }
+        public static MyDoubleType operator *(MyDoubleType myDouble, double tal)
+        {
+            var result = new MyDoubleType(myDouble.DoubleValue * tal);
+            return result;
+        }
+        public static MyDoubleType operator +(MyDoubleType tal, MyDoubleType myDouble)
+        {
+            var result = new MyDoubleType(tal.DoubleValue + myDouble.DoubleValue);
+            return result;
+        }
+        public static MyDoubleType operator -(MyDoubleType tal, MyDoubleType myDouble)
+        {
+            var result = new MyDoubleType(tal.DoubleValue - myDouble.DoubleValue);
+            return result;
+        }
+        public static MyDoubleType operator /(MyDoubleType tal, MyDoubleType myDouble)
+        {
+            var result = new MyDoubleType(tal.DoubleValue / myDouble.DoubleValue);
+            if (tal.DoubleValue != 0)
+            {
+                return result;
+            }
+            else
+                throw new Exception("Can't be divided by zero");
+        }
+        public static MyDoubleType operator *(MyDoubleType tal, MyDoubleType myDouble)
+        {
+            var result = new MyDoubleType(tal.DoubleValue * myDouble.DoubleValue);
+            return result;
+        }
+        public static bool operator ==(MyDoubleType myDouble, MyDoubleType tal)
+        {
+            var result = (myDouble.DoubleValue == tal.DoubleValue);
+            return result;
+        }
+        public static bool operator !=(MyDoubleType myDouble, MyDoubleType tal)
+        {
+            var result = (myDouble.DoubleValue != tal.DoubleValue);
+            return result;
+        }
+        public static bool operator <(MyDoubleType myDouble, MyDoubleType stuff)
+        {
+            var result = (myDouble.DoubleValue < stuff.DoubleValue);
+            return result;
+        }
+        public static bool operator >(MyDoubleType myDouble, MyDoubleType myDouble2)
+        {
+            var result = (myDouble.DoubleValue > myDouble2.DoubleValue);
+            return result;
+        }
+        public static bool operator <=(MyDoubleType myDouble, MyDoubleType tal)
+        {
+            var result = (myDouble.DoubleValue <= tal.DoubleValue);
+            return result;
+        }
+        public static bool operator >=(MyDoubleType myDouble, MyDoubleType tal)
+        {
+            var result = (myDouble.DoubleValue >= tal.DoubleValue);
+            return result;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{DoubleValue}");
+        }
+    }
+}
---
+++ Kurs C#/laborationAkwasiKarikari/Lab6/Point3D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Point3D
+    {
+        private int x;
+        private int y;
+        private int z;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public int Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public override string ToString()
+        {
+            return $"X: {X}\r\nY: {Y}\r\nZ: {Z}";
+        }
+    }
+}
